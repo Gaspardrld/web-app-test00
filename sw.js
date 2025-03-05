e847bb4b0472c056e841e0e33753a1c5e5801381
Gaspardrld <126902802+Gaspardrld@users.noreply.github.com>
2025-03-05 23:03:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,39 @@
-const CACHE_NAME = "todo-list-cache-v1";
+const CACHE_NAME = "karpov-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
   "/script.js",
   "/manifest.json",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Mise en cache...");
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Mise en cache des fichiers...");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
